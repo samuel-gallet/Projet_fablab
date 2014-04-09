75a7c31d6a91e01261b6f7b6882453ab1c585728
Caroline Thomet <caroline.thomet@phelma.grenoble-inp.fr>
2014-04-09 15:59:18 +0200

    Maintenant on a le droit de sortir de la maison + qqes améliorations

--- code_arduino/capteur_presence/capteur_presence/capteur_presence.ino
+++ code_arduino/capteur_presence/capteur_presence/capteur_presence.ino
@@ -2,7 +2,7 @@ int val;
 int capteur = 3;
 boolean enAttente;
 unsigned long curr_time, prev_time;
-int personne;
+//int personne;
 
 
 void setup ()
@@ -13,12 +13,13 @@ void setup ()
   curr_time = 0;
   prev_time = 0;
   val = 0;
-  personne = 0;
+ // personne = 0;
 
 }
 
 void loop()
 {
+  // Capteur entrée
   char command[500];
   int i = 0;
   if (Serial.readBytes(command, 500) == 0)
@@ -45,20 +46,28 @@ void loop()
   {
     prev_time = curr_time;
     curr_time = millis();
-    while (prev_time + 10000 > curr_time)
+    while (prev_time + 5000 > curr_time)
     {
       curr_time = millis();
       val = digitalRead(capteur);
       if (val == 1) {
-        personne ++;
-        for(int i = 0; i < 5; i++)
-          sendMessage("Personne=", personne);
+        //personne ++;
+        Serial.println("Personne +1");
         break;
       }
     }
     enAttente = false;
-
   } 
+  
+  // Capteur sortie
+  val = digitalRead(capteur);
+  if(val == 1) 
+  {
+    delay(500);
+    Serial.println("attente sortie");
+    delay(5000);
+  }
+  
    
 }    
 
--- code_arduino/paillasson_resistif/paillasson_resistif.ino
+++ code_arduino/paillasson_resistif/paillasson_resistif.ino
@@ -1,31 +1,82 @@
 float val;
 int led = 9;
 int resistance= A0; // pin analog A0
+boolean enAttente;
+unsigned long curr_time, prev_time;
 
 
 void setup ()
 {
   Serial.begin(9600);
-  
   pinMode(resistance, INPUT);
   pinMode(led, OUTPUT);
+  enAttente = false;
+  curr_time = 0;
+  prev_time = 0;
 }
 
 void loop()
 { 
+  // Détection poids entrée
   val = analogRead(resistance);
   
-  if(val > 600) 
+  if(val > 550) 
   {
     analogWrite(led, 0);
   } else 
   {
     delay(500);
     sendMessage("Poids=", val);
-    delay(5000);
+    delay(1000);
     analogWrite(led, val);
   }
+  
   delay(100);
+  
+  // Détection poids sortie
+  char command[500];
+  //int i = 0;
+  if (Serial.readBytes(command, 500) == 0)
+  {
+    command == NULL;
+  } else 
+  {
+    char *p = command;
+    char *str;
+    while ((str = strtok_r(p, ";", &p)) != NULL)
+    {
+      if (strstr(str, "attente paillasson") > 0)
+      {
+        enAttente = true;
+      }/* else
+      {      
+        enAttente = false;
+      }*/
+    }
+  }
+ // delay(500);
+  curr_time = millis(); 
+  if (enAttente)
+  {
+    prev_time = curr_time;
+    curr_time = millis();
+    while (prev_time + 5000 > curr_time)
+    {
+      curr_time = millis();
+      val = analogRead(resistance);
+      if (val < 550) {
+        // personne --;
+        Serial.println("sortie de la maison");
+        break;
+      }
+    }
+    enAttente = false;
+
+  } 
+  
+  
+  
+  
 }
 
 
