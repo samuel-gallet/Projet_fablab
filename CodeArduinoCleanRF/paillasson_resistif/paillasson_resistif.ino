2d3ebde1e86743a34f2d3180275b63ba855498e8
Samuel Gallet <Samuel.Gallet@phelma.grenoble-inp.fr>
2014-04-25 12:51:36 +0200

    Implémentation IA sur arduino

--- CodeArduinoCleanRF/paillasson_resistif/paillasson_resistif.ino
+++ CodeArduinoCleanRF/paillasson_resistif/paillasson_resistif.ino
@@ -1,4 +1,7 @@
 #include <VirtualWire.h>
+#include <VirtualWire_Config.h>
+
+
 
 byte val;
 int resistance= A0; // pin analog A0
@@ -9,7 +12,8 @@ void setup ()
   Serial.begin(9600);
   pinMode(resistance, INPUT);
   // Initialise the IO and ISR
-  vw_set_ptt_pin(transmit_en_pin);
+  //vw_set_ptt_pin(transmit_en_pin);
+  vw_set_tx_pin(transmit_en_pin);
   // Débit (2000b/s)
   vw_setup(2000) ;
 }
@@ -18,14 +22,14 @@ void loop()
 { 
   // Détection poids entrée
   val = analogRead(resistance);
-  Serial.println(val);
+  //Serial.println(val);
   
-  if(val <400){
-    char msg[12] = {'p','a','i','l','l','a','s','s','o','n',' ','#'};
+  if(val > 190){
+    char msg[13] = {'p','a','i','l','l','a','s','s','o','n',' ','#', ';'};
     msg[11]=val;
-    vw_send((uint8_t *)msg, 12);  
+    vw_send((uint8_t *)msg, 13);  
     vw_wait_tx(); // Wait until the whole message is gone
-    delay(1000);
+    delay(2000);
   }     
 }
 
--- CodeArduinoCleanRF/recepteurRF/RecepteurRF_ino/RecepteurRF_ino.ino
+++ CodeArduinoCleanRF/recepteurRF/RecepteurRF_ino/RecepteurRF_ino.ino
@@ -8,6 +8,10 @@ HRCSwitch mySwitch = HRCSwitch();
 
 const int receive_pin = 4;
 const int transmit_pin = 9;
+boolean attenteCapteur;
+boolean attentePaillasson;
+int cptPersonne;
+unsigned long long t;
 
 void setup()
 {
@@ -19,22 +23,49 @@ void setup()
     vw_rx_start();       // Start the receiver PLL running
     mySwitch.enableTransmit(transmit_pin);
     mySwitch.switchOff(2, 2);
+    attenteCapteur = false;
+    attentePaillasson = false;
+    cptPersonne = 0;
 }
 
 void loop()
 {
     uint8_t buf[VW_MAX_MESSAGE_LEN];
     uint8_t buflen = VW_MAX_MESSAGE_LEN;
-
     if (vw_get_message(buf, &buflen)) // Non-blocking
     {
-	int i;
-	for (i = 0; i < buflen; i++)
-	{
-	    Serial.print((char)buf[i]);
-	}
-	Serial.println();
+      
+      manageCommand((char*)buf);
+      if (attenteCapteur)
+      {
+        t = millis();
+        while (t + 5000 > millis())
+        {
+          if (vw_get_message(buf, &buflen))
+          {
+            manageCommand((char *)buf);
+            if (!attenteCapteur)
+              break;
+          }
+        }
+        attenteCapteur = false;
+      }
+      if (attentePaillasson)
+      {
+        t = millis();
+        while (t + 5000 > millis())
+        {
+          if (vw_get_message(buf, &buflen))
+          {
+            manageCommand((char *)buf);
+            if (!attentePaillasson)
+              break;
+          }
+        }
+        attentePaillasson = false;
+      }
     }
+    
 }
 
 /*
@@ -42,3 +73,46 @@ void loop()
 	Grace à un switch et des variables globales.
 	
 */
+
+void manageCommand(char * command) 
+{
+  if (strstr(command, "capteur;"))
+  {
+    if (attenteCapteur) 
+    {
+      Serial.println("entree");
+      attenteCapteur = false;
+      cptPersonne++;
+      Serial.print("nb Personne = ");
+      Serial.print(cptPersonne);
+      Serial.println();
+    }
+    else if (cptPersonne == 0) 
+    {
+      Serial.println("intrusion");
+    } else
+    {
+      attentePaillasson = true;
+    }
+  }
+  if (strstr(command, "paillasson "))
+  {
+    if (attentePaillasson) 
+    {
+      Serial.print("sortie ");
+      Serial.print((uint8_t)command[11]);
+      Serial.println();
+      attentePaillasson = false;
+      cptPersonne --;
+      Serial.print("nb Personne = ");
+      Serial.print(cptPersonne);
+      Serial.println();
+    }
+    else
+    {
+      attenteCapteur = true;
+    }
+  }   
+}
+
+
